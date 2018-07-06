e7b3c9cc29a17b92193b077e9a882339ceccb45b
public-darjeeling <twitter.darjeeling@gmail.com>
2018-07-06 23:43:03 +0900

    2018_0706 23:42

---
+++ init.js
@@ -0,0 +1,12 @@
+function init(){
+	document.getElementById('title').innerHTML = 
+	"Public Darjeeling Github.io!!";
+	document.getElementById('logo').innerHTML = 
+	"<div style=\"font-size :50px;font-family: 'Cambria Math','ヒラギノ明朝 ProN','Hiragino Mincho ProN',serif;\">Darjeeling's Studio</div><br>" + 
+	"<div style=\"font-family: 'Cambria Math','ヒラギノ明朝 ProN','Hiragino Mincho ProN',serif;\">2進数からAIまで、好上心を持って創作します！</div>";
+	document.getElementById('newsList').innerHTML = 
+	"<h2>News List</h2>" + 
+	"<div><a href=\"BuildSimulator.html\">ビルドシミュレーター for War Song</a>" + 
+	"<div><a href=\"KanColleSimulator.html\">艦これシミュレーター</a>" + 
+	"<div><a href=\"GenerationAlgorithm.html\">遺伝的アルゴリズム(OneMax問題)</a>";
+}
--- newsList.js
+++
@@ -1,7 +0,0 @@
-function init(){
-	document.getElementById('newsList').innerHTML = 
-	"<h2>News List</h2>" + 
-	"<div><a href=\"BuildSimulator.html\">ビルドシミュレーター for War Song</a>" + 
-	"<div><a href=\"KanColleSimulator.html\">艦これシミュレーター</a>" + 
-	"<div><a href=\"GenerationAlgorithm.html\">遺伝的アルゴリズム(OneMax問題)</a>";
-}
