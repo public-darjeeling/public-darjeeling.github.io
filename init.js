077937005310cb47c602701c019a7fc7ceff3635
public-darjeeling <twitter.darjeeling@gmail.com>
2018-07-07 11:59:11 +0900

    2018_0707 11:59

--- init.js
+++ init.js
@@ -2,8 +2,8 @@ function init(){
 	document.getElementById('title').innerHTML = 
 	"Public Darjeeling Github.io!!";
 	document.getElementById('logo').innerHTML = 
-	"<div style=\"font-size :50px;font-family: 'Cambria Math','ヒラギノ明朝 ProN','Hiragino Mincho ProN',serif;\">Darjeeling's Studio</div><br>" + 
-	"<div style=\"font-family: 'Cambria Math','ヒラギノ明朝 ProN','Hiragino Mincho ProN',serif;\">2進数からAIまで、好上心を持って創作します！</div>";
+	"<div class=\"mainTitle\">Darjeeling's Studio</div><br>" + 
+	"<div class=\"subTitle\">2進数からAIまで、好上心を持って創作します！</div>";
 	document.getElementById('newsList').innerHTML = 
 	"<h2>News List</h2>" + 
 	"<div><a href=\"BuildSimulator.html\">ビルドシミュレーター for War Song</a>" + 
