bb6c82acd2e711d39dc090723ddfd1596ebb4c91
public-darjeeling <twitter.darjeeling@gmail.com>
2018-07-09 22:20:06 +0900

    2018_0709 22:20

--- graph.js
+++ graph.js
@@ -2,18 +2,18 @@ var canvas;
 let ctx;
 
 canvas = document.getElementById('canvas');
-	ctx = canvas.getContext('2d');
-	ctx.strokeRect(0,0,400,400);
+ctx = canvas.getContext('2d');
+ctx.strokeRect(0,0,400,400);
 
-	ctx.beginPath();
-	ctx.moveTo(0,200);
-	ctx.lineTo(400,200);
-	ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(0,200);
+ctx.lineTo(400,200);
+ctx.stroke();
 
-	ctx.beginPath();
-	ctx.moveTo(200,0);
-	ctx.lineTo(200,400);
-	ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(200,0);
+ctx.lineTo(200,400);
+ctx.stroke();
 
 
 function onClick(){
@@ -31,16 +31,16 @@ function onClick(){
 	ctx.lineTo(200,400);
 	ctx.stroke();
 
-	let xxx = document.getElementById('xxx').value;
-	let xx = document.getElementById('xx').value;
-	let x = document.getElementById('x').value;
-	let n = document.getElementById('n').value;
+	let xxx = parseFloat(document.getElementById('xxx').value);
+	let xx = parseFloat(document.getElementById('xx').value);
+	let x = parseFloat(document.getElementById('x').value);
+	let n = parseFloat(document.getElementById('n').value);
 
 	ctx.lineWidth = 2; //線の太さ
     for(i = -50;i < 50;i++){
 	    ctx.beginPath(); //パスの描画を始める
 
-	    console.log("x : " + (i * 4) + " y : " + ((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * 4) * 0.1);
+	    console.log("x : " + (i * 4) + " y : " + (xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * 0.4);
         ctx.moveTo(i * 4 + 199,((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * -0.4) + 200);
         ctx.lineTo(i * 4 + 201,((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * -0.4) + 200);
         ctx.stroke(); //線の終了
