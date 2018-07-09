fe1f317e3fd407dbe4921e0fe5aac260954596c2
public-darjeeling <twitter.darjeeling@gmail.com>
2018-07-09 17:03:46 +0900

    2018_0709 17:03

---
+++ graph.js
@@ -0,0 +1,76 @@
+var canvas;
+let ctx;
+
+canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+	ctx.strokeRect(0,0,400,400);
+
+	ctx.beginPath();
+	ctx.moveTo(0,200);
+	ctx.lineTo(400,200);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(200,0);
+	ctx.lineTo(200,400);
+	ctx.stroke();
+
+
+function onClick(){
+	ctx.clearRect(0,0,400,400);
+	
+	ctx.strokeRect(0,0,400,400);
+
+	ctx.beginPath();
+	ctx.moveTo(0,200);
+	ctx.lineTo(400,200);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(200,0);
+	ctx.lineTo(200,400);
+	ctx.stroke();
+
+	let xxx = document.getElementById('xxx').value;
+	let xx = document.getElementById('xx').value;
+	let x = document.getElementById('x').value;
+	let n = document.getElementById('n').value;
+
+	ctx.lineWidth = 2; //線の太さ
+    for(i = -50;i < 50;i++){
+	    ctx.beginPath(); //パスの描画を始める
+
+	    console.log("x : " + (i * 4) + " y : " + ((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * 4) * 0.1);
+        ctx.moveTo(i * 4 + 199,((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * -0.4) + 200);
+        ctx.lineTo(i * 4 + 201,((xxx * Math.pow(i,3) + xx * Math.pow(i,2) + x * Math.pow(i,1) + n) * -0.4) + 200);
+        ctx.stroke(); //線の終了
+	}
+
+}
+
+class Button {
+	constructor (x, y, width, height, text) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.text = text;
+	}
+
+	install(canvas){
+		var ctx = canvas.getContext('2d');
+		ctx.strokeRect(this.x,this.y,this.width,this.height);
+		ctx.fillText(this.text,(this.x + this.x + this.width) / 2,(this.y + this.y + this.height) / 2);
+	}
+
+	onClick(e){
+		var mouseX = e.clientX - canvas.offsetLeft;
+		var mouseY = e.clientY - canvas.offsetTop;
+		if(this.x <= mouseX && mouseX <= this.width + this.x && this.y <= mouseY && mouseY <= this.height + this.y)
+			return true;
+		else
+			return false;
+	}
+}
+
+// https://html5experts.jp/takazudo/17355/
